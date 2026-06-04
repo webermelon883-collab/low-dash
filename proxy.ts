77e63daced5fd21ddd7826b61c2028e42f518629
Rijwan <rijwan@gmail.com>
2026-06-04 13:49:37 +0600

    changes

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,8 @@
 import { withAuth } from "next-auth/middleware";
 import { NextRequest } from "next/server";
 
-export const middleware = withAuth(
-  function middleware(req: NextRequest & { nextauth: any }) {
+export const proxy = withAuth(
+  function proxy(req: NextRequest & { nextauth: any }) {
     // Settings page - only admin can access
     if (req.nextUrl.pathname === "/settings") {
       const token = req.nextauth.token;
